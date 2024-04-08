0c6783455f94e9ab4002f8ebc26ed613472cc78c
Bryan31542 <00063820@uca.edu.sv>
2024-04-08 14:48:20 -0600

    :sparkles: Feat: add jwt

---
+++ src/config/jwt.ts
@@ -0,0 +1,18 @@
+import jwt from 'jsonwebtoken'
+
+export class JwtAdapter {
+  static async generateToken(
+    payload: Object,
+    duration: string = '2h'
+  ): Promise<string | null> {
+    return new Promise(resolve => {
+      // todo: seed
+      jwt.sign(payload, 'SEED', { expiresIn: duration }, (err, token) => {
+        if (err) {
+          return resolve(null)
+        }
+        return resolve(token!)
+      })
+    })
+  }
+}
--- src/presentation/auth/controller.ts
+++ src/presentation/auth/controller.ts
@@ -1,9 +1,19 @@
 import { Request, Response } from 'express'
-import { AuthRepository, RegisterDto } from '../../domain'
+import { AuthRepository, CustomError, RegisterDto } from '../../domain'
+import { JwtAdapter } from '../../config'
 
 export class AuthController {
   // DI
   constructor(private readonly authRepository: AuthRepository) {}
+
+  private handleError = (error: unknown, res: Response) => {
+    if (error instanceof CustomError) {
+      return res.status(error.statusCode).json({ error: error.message })
+    }
+
+    console.error(error)
+    return res.status(500).json({ error: 'Internal server error' })
+  }
 
   loginUser = (req: Request, res: Response) => {
     res.json({ message: 'Login' })
@@ -18,7 +28,12 @@ export class AuthController {
 
     this.authRepository
       .register(registerDto!)
-      .then(user => res.json(user))
-      .catch(error => res.status(500).json(error))
+      .then(async user => {
+        res.json({
+          user,
+          token: await JwtAdapter.generateToken({ id: user.id })
+        })
+      })
+      .catch(error => this.handleError(error, res))
   }
 }
