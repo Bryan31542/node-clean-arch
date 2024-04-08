2f1058437dcae4816dbdd363f4a14ed580333f7f
Bryan31542 <00063820@uca.edu.sv>
2024-04-08 12:47:48 -0600

    :sparkles: Feat: connect to mongo

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,20 @@
+import { envs } from './config'
+import { MongoDatabase } from './data/mongodb'
 import { AppRoutes } from './presentation/routes'
 import { Server } from './presentation/server'
 
-;(() => {
+(() => {
   main()
 })()
 
 async function main() {
+  await MongoDatabase.connect({
+    mongoUrl: envs.MONGO_URL,
+    dbName: envs.MONGO_DB_NAME
+  })
+
   new Server({
-    port: 8080, // envs.PORT
+    port: envs.PORT,
     routes: AppRoutes.routes
   }).start()
 }
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,3 @@
-//export * from './envs'
+export * from './envs'
 
 export * from './validators'
